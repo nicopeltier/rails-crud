df5bae73d0fee6f92b0bb8af2f7e1e9186774dd1
nicolas peltier <nicolas.peltier@gmail.com>
2022-08-02 22:19:34 +0200

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "restaurants/new", to: "restaurants#new"
   get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show"
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
 end
