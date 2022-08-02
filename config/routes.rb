74db74dec4d3014a3f4907a2e016f9c6112c7f9b
nicolas peltier <nicolas.peltier@gmail.com>
2022-08-02 23:43:16 +0200

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "restaurants/new", to: "restaurants#new"
   get "restaurants", to: "restaurants#index"
-  post "restaurants", to: "restaurants#create"
+  get "restaurants/new", to: "restaurants#new"
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
-
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
 end
