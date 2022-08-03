290048992ddbac57df0f7e9e1b9722ddd9909ef1
nicolas peltier <nicolas.peltier@gmail.com>
2022-08-03 10:37:55 +0200

    final

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
-
+    before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 def index
     @restaurants = Restaurant.all 
 end
 
 def show 
-   @restaurant =  Restaurant.find(params[:id])
+   
 end
 
 def new
@@ -13,30 +13,29 @@ def new
 end
 
 def create
-    @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
     redirect_to restaurant_path(@restaurant)
 end
 
 def edit
-    @restaurant = Restaurant.find(params[:id])
 end
 
 def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     redirect_to restaurant_path(@restaurant)  
 end
 
 def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
-    redirect_to restaurant_path, status: :see_other
+    redirect_to restaurants_path, status: :see_other
 end
 
 
 private
 
+def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 def restaurant_params
     params.require(:restaurant).permit(:name, :address, :rating)
 end
